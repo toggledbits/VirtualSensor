24a1c0bdce05e2c831de0490e2e62e2f5b80c1d2
Patrick Rigney <patrick@toggledbits.com>
2019-03-17 14:07:42 -0400

    Prep version for release

--- L_VirtualSensor1.lua
+++ L_VirtualSensor1.lua
@@ -9,7 +9,7 @@ module("L_VirtualSensor1", package.seeall)
 
 local _PLUGIN_ID = 9031
 local _PLUGIN_NAME = "VirtualSensor"
-local _PLUGIN_VERSION = "1.5develop"
+local _PLUGIN_VERSION = "1.5"
 local _PLUGIN_URL = "http://www.toggledbits.com/virtualsensor"
 local _CONFIGVERSION = 010202
 
